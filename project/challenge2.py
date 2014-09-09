bb3c011f640932cd5ed172667459adc3e4c28a46
Daniele Annuzzi <annuzzi.daniele@gmail.com>
2014-09-09 22:29:10 +0200

    CH2: Add other algorithms & Add ch2 notebook

--- project/challenge2.py
+++ project/challenge2.py
@@ -13,12 +13,35 @@ print data.columns
 
 filtered_data = data[data.year == 2013]
 filtered_data = filtered_data[['arr_port', 'pax']]
+#filtered_data has the counter row as index being a DataFrame
+#and two series objects: arr_port, pax
 
+#Method 1
+#groupby on the arr_port column and then iterate on groups
 result = {}
 with Timer() as t:
     for arr_port, group in filtered_data.groupby('arr_port'):
         result[arr_port] = group["pax"].sum()
-print "=> top computation: %s s" % t.secs
-print result
-#TODO sort result
+result = sorted(result.items(), key=lambda x:x[1], reverse=True)
+print "=> top computation - 1: %s s" % t.secs
+#for k,v in result[:10]:
+#    print k,v
+
+#Method 2
+# groupby on the arr_port column, then access directly to group with pax as a DataFrame
+# to execute the sum operation
+with Timer() as t:
+    groups = filtered_data.groupby('arr_port')
+    result = groups[['pax']].sum().sort('pax', ascending = False)
+print "=> top computation - 2: %s s" % t.secs
+#print result[:10]
+
+#Method 3
+#use the arr_port as index on rows for the pax Series
+with Timer() as t:
+    data_with_index = filtered_data.set_index('arr_port')
+    groups = data_with_index.groupby(lambda x: x)
+    result = groups.sum().sort('pax', ascending = False)
+print "=> top computation - 3: %s s" % t.secs
+#print result[:10]
 
